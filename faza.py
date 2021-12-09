0d57ad9861a1d8e7edf2863d5057faef2adc6053
viktor <bnd-baza@mail.ru>
2021-12-09 23:39:35 +0800

    first commit

--- faza.py
+++ faza.py
@@ -2,7 +2,7 @@ from models import Point
 
 def Faza_update(order):
   post = []
-  points = Point.filter(assembly__cas__cas = order).order_by(Point.point_y,Point.point_x,Point.point_z)
+  points = Point.filter(assembly__cas__cas = order).order_by(Point.point_z,Point.point_y,Point.point_x)
   line=1
   faza=1
   weight=0
